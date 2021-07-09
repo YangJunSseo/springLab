93dda50881ae458a7bc1dde4342771fca1e3d883
YangJunSseo <ehdwns6157@naver.com>
2021-07-09 09:50:08 +0900

    d

---
+++ BoardWebLab/src/main/java/polymorphism/TV.java
@@ -0,0 +1,19 @@
+package polymorphism;
+
+public interface TV {
+	
+	// interface 구성멤버
+	// 1. 상수
+	// 2. 추상 메소드
+	// 3. 디폴드 메소드
+	// 4. 정적 메소드
+	
+    public void powerOn(); // 추상 메소드
+    
+    public void powerOff();
+    
+    public void volumeUp();
+    
+    public void volumeDown();
+
+}
